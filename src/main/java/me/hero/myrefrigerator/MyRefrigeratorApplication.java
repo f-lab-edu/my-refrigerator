60167bfcc421164ce853f1ad09543dcebe41efb9
Hero <81640511+icraft2170@users.noreply.github.com>
2022-07-30 12:19:30 +0900

    initial setting

---
+++ src/main/java/me/hero/myrefrigerator/MyRefrigeratorApplication.java
@@ -0,0 +1,13 @@
+package me.hero.myrefrigerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRefrigeratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRefrigeratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/hero/myrefrigerator/MyRefrigeratorApplicationTests.java
@@ -0,0 +1,13 @@
+package me.hero.myrefrigerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRefrigeratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
