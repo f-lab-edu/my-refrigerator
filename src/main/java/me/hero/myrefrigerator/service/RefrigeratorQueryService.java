654c5a4f0575c96720c1585d5e7453a08559f327
Hero <81640511+icraft2170@users.noreply.github.com>
2022-08-01 20:18:15 +0900

    feature : #5 Query Service 구현 - QueryService의 역할 -> 조회 로직을 담당한다. - Transactional

--- src/main/java/me/hero/myrefrigerator/service/RefrigeratorQueryService.java
+++ src/main/java/me/hero/myrefrigerator/service/RefrigeratorQueryService.java
@@ -1,24 +1,22 @@
 package me.hero.myrefrigerator.service;
 
 import lombok.RequiredArgsConstructor;
-import me.hero.myrefrigerator.domain.Item;
 import me.hero.myrefrigerator.domain.Refrigerator;
 import me.hero.myrefrigerator.repository.RefrigeratorRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class RefrigeratorService {
+public class RefrigeratorQueryService {
     private final RefrigeratorRepository refrigeratorRepository;
 
-    public void createRefrigerator(Refrigerator refrigerator) {
-        refrigeratorRepository.save(refrigerator);
+    public Optional<Refrigerator> findRefrigeratorById(Long refrigeratorId) {
+        return refrigeratorRepository.findById(refrigeratorId);
     }
 
-    public void createItem(Item item) {
-        refrigeratorRepository.saveItem(item);
-    }
 
 }
