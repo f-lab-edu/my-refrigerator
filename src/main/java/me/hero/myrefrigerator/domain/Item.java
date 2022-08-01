e6edc5c8113ebcdb1cdd3484a84ee1052a629e52
Hero <81640511+icraft2170@users.noreply.github.com>
2022-08-01 21:15:20 +0900

    feature : #5 Item 관련 Domain 로직 작성 - addRefrigerator() - validVolume()

--- src/main/java/me/hero/myrefrigerator/domain/Item.java
+++ src/main/java/me/hero/myrefrigerator/domain/Item.java
@@ -1,13 +1,10 @@
 package me.hero.myrefrigerator.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-@NoArgsConstructor @AllArgsConstructor @Builder @Data
+@NoArgsConstructor @AllArgsConstructor @Builder @Getter
 public class Item {
     private Long id;
     private String name;
@@ -15,4 +12,13 @@ public class Item {
     private LocalDateTime storageEndDate;
     private Long volume;
     private Long refrigeratorId;
+
+    public void validVolume(Long remainingCapacity) {
+        if (remainingCapacity < this.volume) throw new RuntimeException();
+    }
+
+    public void addRefrigerator(Long refrigeratorId) {
+        if (refrigeratorId == null) throw new RuntimeException();
+        this.refrigeratorId = refrigeratorId;
+    }
 }
