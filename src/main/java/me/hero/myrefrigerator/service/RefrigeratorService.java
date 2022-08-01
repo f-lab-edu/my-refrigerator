40f88c27aa672d8bffd1e14d9f2fc0a9971da7c2
Hero <81640511+icraft2170@users.noreply.github.com>
2022-08-01 20:13:31 +0900

    feature : #5 Service 계층 구현 - createRefrigerator(Refrigerator refrigerator) - createItem(Item item)

--- src/main/java/me/hero/myrefrigerator/service/RefrigeratorService.java
+++ src/main/java/me/hero/myrefrigerator/service/RefrigeratorService.java
@@ -1,6 +1,8 @@
 package me.hero.myrefrigerator.service;
 
 import lombok.RequiredArgsConstructor;
+import me.hero.myrefrigerator.domain.Item;
+import me.hero.myrefrigerator.domain.Refrigerator;
 import me.hero.myrefrigerator.repository.RefrigeratorRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,14 +11,12 @@ import org.springframework.stereotype.Service;
 public class RefrigeratorService {
     private final RefrigeratorRepository refrigeratorRepository;
 
-    public void createRefrigerator() {
-
+    public void createRefrigerator(Refrigerator refrigerator) {
+        refrigeratorRepository.save(refrigerator);
     }
 
-    public void createItem() {
-
+    public void createItem(Item item) {
+        refrigeratorRepository.saveItem(item);
     }
-
-
 
 }
