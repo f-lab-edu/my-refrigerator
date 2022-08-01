02a19058e7590f288cc8c5d50cb2196c86e9eb9b
Hero <81640511+icraft2170@users.noreply.github.com>
2022-08-01 21:36:36 +0900

    issue : #5 LocalDateTime DateTimeFormat 불일치로 역직력화 되지 않았던 문제 해결 - @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) 어노테이션을 추가하여 Format 지정

--- src/main/java/me/hero/myrefrigerator/controller/dto/RefrigeratorDto.java
+++ src/main/java/me/hero/myrefrigerator/controller/dto/RefrigeratorDto.java
@@ -2,6 +2,7 @@ package me.hero.myrefrigerator.controller.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -18,7 +19,9 @@ public class RefrigeratorDto {
     @NoArgsConstructor
     public static class ItemSaveRequest{
         private String name;
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
         private LocalDateTime storageStartDate;
+        @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
         private LocalDateTime storageEndDate;
         private Long volume;
     }
