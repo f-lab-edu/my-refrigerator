3c06851e6513a6a7d93692be9f7a4fcd55d8ffe3
Hero <81640511+icraft2170@users.noreply.github.com>
2022-08-01 19:59:01 +0900

    test : #5 RefrigeratorService Object 생성 - 메서드 정의   - createRefrigerator(...)   - createItem(...)

---
+++ src/main/java/me/hero/myrefrigerator/service/RefrigeratorService.java
@@ -0,0 +1,22 @@
+package me.hero.myrefrigerator.service;
+
+import lombok.RequiredArgsConstructor;
+import me.hero.myrefrigerator.repository.RefrigeratorRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RefrigeratorService {
+    private final RefrigeratorRepository refrigeratorRepository;
+
+    public void createRefrigerator() {
+
+    }
+
+    public void createItem() {
+
+    }
+
+
+
+}
