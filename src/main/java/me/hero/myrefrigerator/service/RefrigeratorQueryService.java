907e9fc23f4929d5b7b4daa00790ca341c9f37fb
Hero <81640511+icraft2170@users.noreply.github.com>
2022-08-02 21:19:43 +0900

    feature : #7 상품 조회 Service Layer 작성

--- src/main/java/me/hero/myrefrigerator/service/RefrigeratorQueryService.java
+++ src/main/java/me/hero/myrefrigerator/service/RefrigeratorQueryService.java
@@ -1,18 +1,32 @@
 package me.hero.myrefrigerator.service;
 
 import lombok.RequiredArgsConstructor;
+import me.hero.myrefrigerator.domain.Item;
+import me.hero.myrefrigerator.domain.dto.ItemResponse;
+import me.hero.myrefrigerator.repository.RefrigeratorQueryRepository;
 import me.hero.myrefrigerator.repository.RefrigeratorRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class RefrigeratorQueryService {
     private final RefrigeratorRepository refrigeratorRepository;
+    private final RefrigeratorQueryRepository refrigeratorQueryRepository;
 
 
     public Long findRemainingCapacityById(Long refrigeratorId) {
         return refrigeratorRepository.findRemainingCapacityById(refrigeratorId);
+    }
+
+    public List<Item> findAllByRefrigerator(Long refrigeratorId) {
+        return refrigeratorQueryRepository.findAllItemsByRefrigerator(refrigeratorId);
+    }
+
+    public List<ItemResponse> findAllItems() {
+        return refrigeratorQueryRepository.findItemsOrderByRefrigerator();
     }
 }
