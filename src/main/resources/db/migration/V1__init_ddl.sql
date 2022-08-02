d88fd6eb560e478e4b9c4bb9b124b004f5af5ec6
Hero <81640511+icraft2170@users.noreply.github.com>
2022-08-02 22:48:35 +0900

    #11 기본 데이터 세팅

--- src/main/resources/db/migration/V1__init_ddl.sql
+++ src/main/resources/db/migration/V1__init_ddl.sql
@@ -20,6 +20,36 @@ CREATE TABLE item (
        storage_end_date DATETIME NULL,
        volume BIGINT NOT NULL,
        refrigerator_id BIGINT NOT NULL,
-       CONSTRAINT item_PK PRIMARY KEY (id)
+       CONSTRAINT item_PK PRIMARY KEY (id),
+      CONSTRAINT `item_FK` FOREIGN KEY (`refrigerator_id`) REFERENCES `refrigerator` (`id`)
 ) ENGINE=InnoDB charset = utf8mb4;
 
+#냉장고 데이터
+INSERT INTO refrigerator.refrigerator
+(id, name, created_time, modified_time, deleted_time, limited_volume)
+VALUES(1, '냉장고A', '2022-08-01 21:24:31', '2022-08-01 21:24:31', NULL, 100);
+INSERT INTO refrigerator.refrigerator
+(id, name, created_time, modified_time, deleted_time, limited_volume)
+VALUES(2, '냉장고B', '2022-08-02 22:30:31', '2022-08-02 22:30:31', NULL, 200);
+#아이템 데이터
+INSERT INTO refrigerator.item
+(id, name, storage_start_date, storage_end_date, volume, refrigerator_id)
+VALUES(1, '아이템A', '2022-07-31 00:00:00', '2022-08-29 00:00:00', 30, 1);
+INSERT INTO refrigerator.item
+(id, name, storage_start_date, storage_end_date, volume, refrigerator_id)
+VALUES(2, '아이템B', '2022-07-31 00:00:00', '2022-08-29 00:00:00', 30, 1);
+INSERT INTO refrigerator.item
+(id, name, storage_start_date, storage_end_date, volume, refrigerator_id)
+VALUES(3, '아이템C', '2022-07-31 00:00:00', '2022-08-29 00:00:00', 30, 1);
+INSERT INTO refrigerator.item
+(id, name, storage_start_date, storage_end_date, volume, refrigerator_id)
+VALUES(4, '아이템D', '2022-07-31 00:00:00', '2022-08-29 00:00:00', 10, 2);
+INSERT INTO refrigerator.item
+(id, name, storage_start_date, storage_end_date, volume, refrigerator_id)
+VALUES(5, '아이템E', '2022-08-02 00:00:00', '2022-08-27 00:00:00', 30, 2);
+INSERT INTO refrigerator.item
+(id, name, storage_start_date, storage_end_date, volume, refrigerator_id)
+VALUES(6, '아이템F', '2022-08-03 00:00:00', '2022-08-28 00:00:00', 20, 2);
+INSERT INTO refrigerator.item
+(id, name, storage_start_date, storage_end_date, volume, refrigerator_id)
+VALUES(7, '아이템G', '2022-08-04 00:00:00', '2022-08-29 00:00:00', 40, 2);
